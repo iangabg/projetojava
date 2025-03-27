a619c0271a05a0c2810b68a87687867a12e86531
iangabg <xxluiz1227@gmail.com>
2025-03-27 19:55:31 -0300

    feat: class created for the use of (do-while)

--- src/VerificarSenha.java
+++ src/VerificarSenha.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class VerificarSenha {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String senha;
+
+        do {
+            System.out.println("Digite a senha: ");
+            senha = scanner.nextLine();
+
+            if (!senha.equals("Java21")) {
+                System.out.println("Senha incorreta. Tente novamente");
+            }
+        } while (!senha.equals("Java21"));
+
+        System.out.println("Acesso permitido!");
+        scanner.close();
+    }
 }
