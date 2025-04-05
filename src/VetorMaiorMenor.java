723406ae0f971101a2cdc99d20f7a33001fb2b8d
iangabg <xxluiz1227@gmail.com>
2025-04-05 08:31:49 -0300

    feat: class created Vetor Maior e Menor

--- src/VetorMaiorMenor.java
+++ src/VetorMaiorMenor.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class VetorMaiorMenor {
+    public static void main(String[] args) {
+
+        int[] vetor = {10, 5, 8, 2, 15, 7};
+
+        int maior = vetor[0];
+        int menor = vetor[0];
+        int soma = 0;
+
+        for (int numero : vetor){
+            if (numero > maior){
+                maior = numero;
+            }
+
+            if (numero < menor){
+                menor = numero;
+
+            }
+
+            soma += numero;
+        }
+
+        double media = (double) soma / vetor.length;
+
+        System.out.println("Maior número: " + maior);
+        System.out.println("Menor número: " + menor);
+        System.out.println("Média: " + media);
+    }
 }
